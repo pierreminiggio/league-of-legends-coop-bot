8f8f24b26f8c18ef15aa46b3c31e530353d8c9f4
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-06-24 22:07:44 +0200

    handle pre game dodge timer

--- index.js
+++ index.js
@@ -16,11 +16,14 @@ class Element
     }
 }
 
-const play = {
+const launch = {
     button: new Element(screenSize.width - 1050, screenSize.height - 780),
     page: {
         beginner: new Element(screenSize.width - 800, screenSize.height - 400),
         confirm: new Element(screenSize.width - 720, screenSize.height - 270)
+    },
+    colors: {
+        dodgeTimer: ['525250']
     }
 }
 
@@ -29,10 +32,20 @@ const queue = {
     colors: {
         inQueue: ['0f1a18'],
         dodgeTimer: ['040606'],
-        gameFound: ['c6f2f3'],
+        gameFound: ['c6f2f3', '9cbebe'],
         gameAccepted: ['545451'],
         champSelect: ['000000', '02060a']
     }
+}
+
+/**
+ * 
+ * @param {int} ms
+ * 
+ * @returns {Promise}
+ */
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 /**
@@ -55,43 +68,52 @@ function getColor(element) {
 let lastState = null;
 let color = null;
 
-function startQueuing() {
-    clickButton(play.button)
+function createGame() {
+    clickButton(launch.button)
     setTimeout(() => {
-        clickButton(play.page.beginner)
-        clickButton(play.page.confirm)
-        setTimeout(() => {
-            clickButton(play.page.confirm)
-            console.log('Start queuing !')
-            setInterval(() => {
-                color = getColor(queue.accept)
-                console.log('Color : ' + color)
-                if (queue.colors.inQueue.includes(color)) {
-                    console.log('In queue...')
-                } else if (queue.colors.dodgeTimer.includes(color)) {
-                    console.log('In dodge timer...')
-                } else if (queue.colors.gameFound.includes(color)) {
-                    console.log('Game found !')
-                    clickButton(queue.accept)
-                } else if (queue.colors.gameAccepted.includes(color)) {
-                    console.log('Game Accepted !')
-                } else if (queue.colors.champSelect.includes(color)) {
-                    if (! queue.colors.champSelect.includes(lastState)) {
-                        setTimeout(() => {
-                            playChampSelect()
-                        }, 2000)
-                    }
-                }
-                lastState = color
-            }, 1000)
+        clickButton(launch.page.beginner)
+        clickButton(launch.page.confirm)
+        setTimeout(async () => {
+            while (launch.colors.dodgeTimer.includes(getColor(launch.page.confirm))) {
+                console.log('In dodge timer before queuing...')
+                await sleep(1000)
+            }
+            startQueueing()
         }, 2000)
     }, 1000)
 }
 
-function playChampSelect() {
+function startQueueing()
+{
+    clickButton(launch.page.confirm)
+    console.log('Start queuing !')
+    setInterval(() => {
+        color = getColor(queue.accept)
+        console.log('Color : ' + color)
+        if (queue.colors.inQueue.includes(color)) {
+            console.log('In queue...')
+        } else if (queue.colors.dodgeTimer.includes(color)) {
+            console.log('In dodge timer...')
+        } else if (queue.colors.gameFound.includes(color)) {
+            console.log('Game found !')
+            clickButton(queue.accept)
+        } else if (queue.colors.gameAccepted.includes(color)) {
+            console.log('Game Accepted !')
+        } else if (queue.colors.champSelect.includes(color)) {
+            if (! queue.colors.champSelect.includes(lastState)) {
+                setTimeout(() => {
+                    launchChampSelect()
+                }, 2000)
+            }
+        }
+        lastState = color
+    }, 1000)
+}
+
+function launchChampSelect() {
     console.log('pick a champ')
 }
 
-startQueuing()
+createGame()
 
 
