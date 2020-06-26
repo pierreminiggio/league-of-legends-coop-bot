9ddef4189d91b79e81a7f9fbd03981e4bd342e3c
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2020-06-26 17:34:54 +0200

    add dodge timer color

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const queue = {
     accept: new Element(screenSize.width - 640, screenSize.height - 380),
     colors: {
         inQueue: ['0f1a18', '101b19', '111c1a'],
-        dodgeTimer: ['040606'],
+        dodgeTimer: ['040606', '030605'],
         gameFound: ['c6f2f3', '9cbebe', '8fafb0'],
         gameAccepted: ['545451'],
         champSelect: ['000000', '02060a', '02050a'],
